fe145b6e296a6518da5011a29e6ba9cfc307644a
Alfred Githinji <alfredgithinji87@gmail.com>
2023-01-17 11:44:18 +0300

    Commencing tests

--- models/TutorModel.js
+++ models/TutorModel.js
@@ -6,9 +6,10 @@ const { Schema } = mongoose;
 const TutorSchema = new Schema(
   {
     firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    surname: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
+    contact: { type: String, required: true },
     paid: { type: Boolean, required: true, default: false },
     role: {
       type: String,
